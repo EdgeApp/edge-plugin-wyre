d38ecb2bfb6cedfdfffd392352ab949537378d92
kylanhurt <kylan.hurt@gmail.com>
2019-01-08 14:49:04 -0800

    More detailed info for Wyre fees and timeline, etc

--- src/StartScene.js
+++ src/StartScene.js
@@ -156,7 +156,25 @@ class StartScene extends Component<StartSceneProps, StartSceneState> {
         <div>
           <StartHeader text="Wyre" classes={classes} />
           <StartParagraph classes={classes}>
-            Wyre is a secure and compliant bridge between fiat currencies and cryptocurrency. Exchange crypto safely and securely.
+            Wyre is a compliant fiat to crypto exchange allowing user to safely buy and sell cryptocurrency with a bank account directly from Edge.
+          </StartParagraph>
+        </div>
+        <Divider className={classes.divider} />
+        <div>
+          <StartHeader text="Fee" classes={classes} />
+          <StartParagraph classes={classes}>
+          The following fees are applied for buying and selling cryptocurrency with Wyre:
+            <ul className={classes.feeList}>
+              <li>Edge Wallet 0.5%</li>
+              <li>Wyre 0.5%</li>
+            </ul>
+          </StartParagraph>
+        </div>
+        <Divider className={classes.divider} />
+        <div>
+          <StartHeader text="Time" classes={classes} />
+          <StartParagraph classes={classes}>
+            Initial account verification usually takes 1 business day and transactions settle in about 3 business days.
           </StartParagraph>
         </div>
         <Divider className={classes.divider} />
