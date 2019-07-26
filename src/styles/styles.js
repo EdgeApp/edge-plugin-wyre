4487f6ef23bb261812fead4c9349c91e4be930ec
Allen Hujsak <allen@missionvi.com>
2019-07-26 04:42:52 -0400

    remove xtra margin on button style

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -11,8 +11,7 @@ export const buttonStyle = {
   textTransform: 'none',
   paddingRight: '15px',
   paddingLeft: '15px',
-  margin: '5px 0',
-  borderRadius: '5px',
+  borderRadius: '5px'
 }
 export const buttonStyleHollow = {
   textTransform: 'none',
